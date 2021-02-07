f15659858e11e0a2df05c283045a0588cd6feed9
Diana Dorogina <51213154+DDianka@users.noreply.github.com>
2021-02-08 00:36:04 +0300

    исправления

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 function getRandomInt(min, max) {
-  if (min < max && min >= 0) {
+  if (min <= max && min >= 0) {
     return Math.floor(Math.random(min,max) * (max - min) + min);
-  } return alert('Минимальное число больше максимального, введите другое.');
+  } return alert('Введённые числа не соответствуют требованиям.');
 }
 
 getRandomInt(0,5);
 
 function checkStringLength(string, maxLength) {
-  return string.length < maxLength;
+  return string.length <= maxLength;
 }
 
 checkStringLength('Hello world', 140);
