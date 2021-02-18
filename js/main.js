50e97db5c1d64885425e0bcb6d2e1dcce51d969f
Diana Dorogina <51213154+DDianka@users.noreply.github.com>
2021-02-18 22:16:06 +0300

    массивы фото и комментов

--- js/main.js
+++ js/main.js
@@ -1,13 +1,77 @@
-function getRandomInt(min, max) {
+const getRandomInt = (min, max) => {
   if (min <= max && min >= 0) {
     return Math.floor(Math.random(min,max) * (max - min) + min);
   } return alert('Введённые числа не соответствуют требованиям.');
 }
 
-getRandomInt(0,5);
-
-function checkStringLength(string, maxLength) {
+const checkStringLength = (string, maxLength) => {
   return string.length <= maxLength;
 }
 
-checkStringLength('Hello world', 140);
+const getRandomElement = (array) => {
+  return array[getRandomInt(0, array.length - 1)];
+}
+
+const names = [
+  'Илинка',
+  'Ян',
+  'Наоми',
+  'Джессика',
+  'Эдриан',
+  'Юлиан',
+  'Майя',
+];
+
+const messages = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const descriptions = [
+  'Это я.',
+  'Ищу вдохновение.',
+  'Отличная книга - рекоммендую',
+  'С друзьями.',
+  'Как вам мой новый имидж?',
+  'Работа выполнена.',
+  'Какое красивое сегодня небо. Как вам?',
+];
+
+let lastCommentId = 0;
+const generateComments = () => {
+  const comments = [];
+  const commentsAmount = getRandomInt(1, 5);
+
+  for (let i = 1; i <= commentsAmount; ++i) {
+    comments.push({
+      id: ++lastCommentId,
+      avatar: 'img/avatar' + '-' + getRandomInt(1, 6) + '.svg',
+      message: getRandomElement(messages),
+      name: getRandomElement(names),
+    });
+  }
+
+  return comments;
+}
+
+const generatePhotosArray = () => {
+  const photos= [];
+
+  for (let i = 1; i <= 25; i++) {
+    photos.push({
+      id: i,
+      url:'photos/' + i + '.jpg',
+      description: getRandomElement(descriptions),
+      likes: getRandomInt(15,200),
+      comment: generateComments(),
+    });
+  }
+
+  return photos;
+}
+
+generatePhotosArray();
