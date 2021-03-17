7747092d7ca319b0ce6058c0a8a797edeac55dcc
Diana Dorogina <51213154+DDianka@users.noreply.github.com>
2021-03-18 00:19:31 +0300

    fixed code appearance

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,11 +1,20 @@
 import { photos } from './data.js';
 
+const bigPicture = document.querySelector('.big-picture');
+const img = bigPicture.querySelector('.big-picture__img img');
+const likes = bigPicture.querySelector('.likes-count');
+const comments = bigPicture.querySelector('.comments-count');
+const fragment = document.createDocumentFragment();
+const comment = bigPicture.querySelector('.social__comment');
+const socialComments = bigPicture.querySelector('.social__comments');
+const socialCommentCount = document.querySelector('.social__comment-count');
+const commentsLoader = document.querySelector('.comments-loader');
+
+
+
 export function handlePicturesContainerClick (evt) {
   const picture = evt.target.closest('.picture');
   const body = document.querySelector('body');
-  const socialCommentCount = document.querySelector('.social__comment-count');
-  const commentsLoader = document.querySelector('.comments-loader');
-
   socialCommentCount.classList.add('hidden');
   commentsLoader.classList.add('hidden');
   body.classList.add('modal-open');
@@ -13,15 +22,6 @@ export function handlePicturesContainerClick (evt) {
   if (picture) {
     const pictureId = Number(picture.dataset.id);
     const photo = photos.find( (elem) => elem.id === pictureId);
-
-    const bigPicture = document.querySelector('.big-picture');
-    const img = bigPicture.querySelector('.big-picture__img img');
-    const likes = bigPicture.querySelector('.likes-count');
-    const comments = bigPicture.querySelector('.comments-count');
-    const fragment = document.createDocumentFragment();
-    const comment = bigPicture.querySelector('.social__comment');
-    const socialComments = bigPicture.querySelector('.social__comments');
-
     bigPicture.classList.remove('hidden');
     img.src = photo.url;
     likes.textContent = photo.likes;
@@ -31,12 +31,10 @@ export function handlePicturesContainerClick (evt) {
     for (let i = 0; i < photo.comments.length; i++) {
       const commentCopy = comment.cloneNode(true);
       const avatar = commentCopy.querySelector('.social__picture');
+      const socialText = commentCopy.querySelector('.social__text');
       avatar.src = photo.comments[i].avatar;
       avatar.alt = photo.comments[i].name;
-
-      const socialText = commentCopy.querySelector('.social__text');
       socialText.textContent = photo.comments[i].message;
-
       fragment.appendChild(commentCopy);
     }
     socialComments.appendChild(fragment);
