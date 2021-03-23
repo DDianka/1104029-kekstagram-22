662c26ae0610e4eb8da15b25c0cbc9554b10e4f6
Diana Dorogina <51213154+DDianka@users.noreply.github.com>
2021-03-23 15:13:34 +0300

    change photo filters

--- js/imageEdit.js
+++ js/imageEdit.js
@@ -2,12 +2,24 @@ const effectSlider = document.querySelector('.effect-level__slider');
 const scaleControlValue = document.querySelector('.scale__control--value');
 const imgUploadScale = document.querySelector('.img-upload__scale');
 
-// const slider = document.querySelector('.effect-level__slider');
-// const effectList = document.querySelector('.effects__list');
-// const effectLevel = document.querySelector('.img-upload__effect-level');
-// const effectLevelValue = document.querySelector('.effect-level__value');
-
+const effectList = document.querySelector('.effects__list');
+const effectLevel = document.querySelector('.img-upload__effect-level');
+const effectLevelValue = document.querySelector('.effect-level__value');
+// const radioBtn = document.querySelector('.effects__radio');
 const photoPreview = document.querySelector('.img-upload__preview').querySelector('img');
+
+effectList.addEventListener('change', function (evt) {
+  const effectsItem = evt.target.closest('.effects__item');
+  const effectsPreview = effectsItem.querySelector('.effects__preview');
+  const effect = effectsPreview.classList[1];
+  photoPreview.className = '';
+  photoPreview.classList.add(effect);
+  if (effectsPreview.classList.contains('effects__preview--none')) {
+    effectLevel.classList.add('hidden');
+  } else {
+    effectLevel.classList.remove('hidden');
+  }
+});
 
 const STEP_SIZE_PHOTO = 25;
 const MIN_PHOTO_SIZE = 25;
@@ -50,4 +62,21 @@ window.noUiSlider.create(effectSlider, {
     max: 100,
   },
   start: 80,
+  step: 1,
+  connect: 'lower',
+  format: {
+    to: function (value) {
+      if (Number.isInteger(value)) {
+        return value.toFixed(0);
+      }
+      return value.toFixed(1);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+});
+
+effectSlider.noUiSlider.on('update', (_, handle, unencoded) => {
+  effectLevelValue.value = unencoded[handle];
 });
--- js/imageUpload.js
+++ js/imageUpload.js
@@ -4,12 +4,18 @@ const body = document.querySelector('body');
 const imageUploadOverlay = document.querySelector('.img-upload__overlay');
 const uploadCancel = document.querySelector('#upload-cancel');
 const uploadFile = document.querySelector('#upload-file');
+const effectLevel = document.querySelector('.img-upload__effect-level');
+const photoPreview = document.querySelector('.img-upload__preview').querySelector('img');
 
 
 
 uploadFile.addEventListener('change', function () {
   imageUploadOverlay.classList.remove('hidden');
   body.classList.add('modal-open');
+  effectLevel.classList.add('hidden');
+  if (photoPreview.classList.contains('effects__preview--none') === false) {
+    photoPreview.className = ('effects__preview--none');
+  }
 })
 
 uploadCancel.addEventListener('click', function () {
